44e09aa5c054ac4aab9f601321f19f68631e42ab
Gauri Joshi <gauri728@gmail.com>
2023-01-08 21:44:50 -0800

    Updates index views and poro attributes

--- app/poro/pokemon.rb
+++ app/poro/pokemon.rb
@@ -1,27 +1,11 @@
 class Pokemon 
-  attr_reader :name, :id, :types, :image, :moves 
+  attr_reader :name, :id, :origin, :image, :shiny
   def initialize(data)
     @name = data[:name]
     @id = data[:id]
-    @types = data[:types]
-    # @image = data[:sprites][:front_default]
-    @moves = data[:moves]
+    @origin = data[:types][0][:type][:name]
+    @image = data[:sprites][:front_default]
+    @shiny = data[:sprites][:front_shiny]
     # require 'pry'; binding.pry 
   end
-
-  # def name_moves 
-  #   name_moves = []
-  #   @moves.each do |move|
-  #     name_moves << move[:move][:name]
-  #   end
-  #   name_moves 
-  # end
-
-  # def types 
-  #   types = []
-  #   @type.each do |t|
-  #     types << t[:type][:name]
-  #   end
-  #   types
-  # end
 end
